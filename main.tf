4cf5a8835c2f0ed8ea983094795ccebc9998b88c
dinukaSanthush <dinukasanthush01@gmail.com>
2022-09-27 20:57:51 -0700

    Added Comments

--- main.tf
+++ main.tf
@@ -1,17 +1,21 @@
+
+# configure gcp infrastructure
+
 provider "google" {
   credentials = file("terraform-363417-5f115e02cd21.json")
-
-  project     = "terraform-363417"
+  project     = "terraform-363417"      
   region      = "asia-southeast1"
   zone        = "asia-southeast1-b"
 }
 
+# create vpc
 resource "google_compute_network" "vpc" {
   name                    = "dinukavpc"
   routing_mode            = "GLOBAL"
   auto_create_subnetworks = true
 }
 
+# allocate a block of private IP addresses for the vpc 
 resource "google_compute_global_address" "private_ip_block" {
   name         = "private-ip-block"
   purpose      = "VPC_PEERING"
@@ -21,12 +25,14 @@ resource "google_compute_global_address" "private_ip_block" {
   network       = google_compute_network.vpc.self_link
 }
 
+# allows instances to communicate using the internal network.
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_block.name]
 }
 
+# firewall rule to allow ssh traffic
 resource "google_compute_firewall" "allow_ssh" {
   name        = "allow-ssh"
   network     = google_compute_network.vpc.name
@@ -39,10 +45,13 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags = ["ssh-enabled"]
 }
 
+# create mysql instance
 resource "google_sql_database" "main" {
   name     = "main"
   instance = google_sql_database_instance.main_primary.name
 }
+
+# add db to private vpc 
 resource "google_sql_database_instance" "main_primary" {
   name             = "main-primary"
   database_version = "MYSQL_8_0"
@@ -56,17 +65,22 @@ resource "google_sql_database_instance" "main_primary" {
     }
   }
 }
+
+#creates mysql user on sql instance
 resource "google_sql_user" "db_user" {
   name     = "dinuka"
   instance = google_sql_database_instance.main_primary.name
   password = "1234"
 }
 
+
+# create gcp instance on the same region as the database (same subnet)
 data "google_compute_subnetwork" "regional_subnet" {
   name   = google_compute_network.vpc.name
   region = "asia-southeast1"
 }
 
+# create instance
 resource "google_compute_instance" "db_proxy" {
   name                      = "db-proxy"
   machine_type              = "e2-medium"
@@ -76,10 +90,13 @@ resource "google_compute_instance" "db_proxy" {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"           
     }
   }
+
+#ssh keys (ssh-keygen)
   metadata = {
     ssh-keys = "${"dinuka"}:${file("~/.ssh/id_rsa.pub")}" 
   }
 
+#add instance to vpc and set to get a public IP
   network_interface {
     network    = google_compute_network.vpc.id
     subnetwork = data.google_compute_subnetwork.regional_subnet.self_link
